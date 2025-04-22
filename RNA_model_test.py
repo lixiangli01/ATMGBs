84547b91ea00be18370efb28cba5adf45af83d2a
lixiangli01 <87192085+lixiangli01@users.noreply.github.com>
2025-04-22 17:24:38 +0800

    Update RNA_model_test.py

--- RNA_model_test.py
+++ RNA_model_test.py
@@ -1,4 +1,4 @@
-# 只用gcn(esm)+gcn(t5)+ca+gcn(esm)+gcn(t5)
+
 import pandas as pd
 import numpy as np
 import torch
@@ -11,7 +11,7 @@ from sklearn.model_selection import KFold
 from sklearn import metrics
 import warnings
 warnings.filterwarnings("ignore",category=DeprecationWarning)
-#from CA import EncoderLayer
+
 
 
 class GraphConvolution(nn.Module):
@@ -42,7 +42,7 @@ class GraphConvolution(nn.Module):
             support = (1-alpha)*hi+alpha*h0
             r = support
         output = theta*torch.mm(support, self.weight)+(1-theta)*r
-        if self.residual: # speed up convergence of the training process
+        if self.residual: 
             output = output+input
         return output
 
@@ -71,10 +71,10 @@ class deepGCN(nn.Module):
             layer_inner = self.act_fn(con(layer_inner,adj,_layers[0],self.lamda,self.alpha,i+1))
         layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)
         
-        #layer_inner = self.fcs[-1](layer_inner)#(layer_inner[mutsite])#可能是在这里
+        
         return layer_inner
 
-#model = GraphPPIS(LAYER, INPUT_DIM, HIDDEN_DIM, NUM_CLASSES, DROPOUT, LAMBDA, ALPHA, VARIANT)
+
 class GraphPPIS(nn.Module):
     def __init__(self, nlayers,  nhidden, nclass, dropout, lamda, alpha, variant):
         super(GraphPPIS, self).__init__()
@@ -87,7 +87,7 @@ class GraphPPIS(nn.Module):
         self.deep_gcn_t5 = deepGCN(nlayers = nlayers, nfeat = 2835, nhidden = nhidden, nclass = nclass,#1024+531
                                 dropout = dropout, lamda = lamda, alpha = alpha, variant = variant)
         
-        #self.Cross_attention =EncoderLayer(128, 128, 0.2, 0.2, 2)
+      
         
         self.deep_gcn_esm_1=deepGCN(nlayers = nlayers, nfeat = 512, nhidden = nhidden, nclass = nclass,
                                 dropout = dropout, lamda = lamda, alpha = alpha, variant = variant)
@@ -98,15 +98,8 @@ class GraphPPIS(nn.Module):
         self.criterion = nn.CrossEntropyLoss() # automatically do softmax to the predicted value and one-hot to the label
         self.optimizer = torch.optim.Adam(self.parameters(), lr = LEARNING_RATE, weight_decay = WEIGHT_DECAY)
 
-        # self.fc = nn.Sequential(nn.Linear(512, 128),
-        #                                 nn.LeakyReLU(0.1),
-        #                                 nn.Dropout(0.1),
-        #                                 nn.Linear(128, 32),
-        #                                 nn.LeakyReLU(0.1),
-        #                                 nn.Dropout(0.1),
-        #                                 nn.Linear(32, 2))
-        # self.Cross_attention =EncoderLayer(512, 512, 0.1, 0.1, 2)
-        self.fc = nn.Sequential(nn.Linear(1024, 256),#256#
+        
+        self.fc = nn.Sequential(nn.Linear(1024, 256),
                                         nn.LeakyReLU(0.1),
                                         nn.Dropout(0.1),
                                         nn.Linear(256, 64),
@@ -115,17 +108,16 @@ class GraphPPIS(nn.Module):
                                         nn.Linear(64, 2))
         self.fc_gcn=nn.Linear(1024, 128)
         self.transformer = nn.TransformerEncoderLayer(
-            d_model=531,#config.hidden_size, 128
-            nhead=3#4
+            d_model=531,
+            nhead=3
         )
         
-    def forward(self, esmfea,t5fea, adj,w_embedding):          # x.shape = (seq_len, FEATURE_DIM); adj.shape = (seq_len, seq_len)
-        #esm_x = esmfea.float()
+    def forward(self, esmfea,t5fea, adj,w_embedding):        
+        
         t5_x = t5fea.float()
         
 
-        # esm_x=torch.squeeze(esm_x)
-        # output_esm = self.deep_gcn_esm(esm_x, adj)  # output.shape = (seq_len, NUM_CLASSES)
+        
         
         t5_x=torch.squeeze(t5_x)
         w_in=w_embedding.float()
@@ -143,9 +135,8 @@ class GraphPPIS(nn.Module):
         return output
 
 
-###################################################################################################
-# dataset
-def norm_dis(mx): # from SPROF
+
+def norm_dis(mx): 
     return 2 / (1 + (np.maximum(mx, 4) / 4))
 
 def normalize(mx):
@@ -159,28 +150,15 @@ def normalize(mx):
 def load_graph(sequence_name):
 
     
-    #dismap = np.load(Feature_Path + "distance_map/" + sequence_name + ".npy")
+   
     try:
         dismap = np.load( "/home/xli/NABProt/task2_NABPs/genT5attenmap/RNA/" + sequence_name + "_attmap.npy")
     except:
         dismap = np.load( "/home/xli/NABProt/task2_NABPs/genT5attenmap/RNA/" + sequence_name + "_attmap.npy")
     dismap=dismap[:-1,:-1]
-    # mask = ((dismap >= 0) * (dismap <= 14))#14
-    # if MAP_TYPE == "d":
-    #     adjacency_matrix = mask.astype(np.int)
-    # elif MAP_TYPE == "c":
-    #     adjacency_matrix = norm_dis(dismap)
-    #     adjacency_matrix = mask * adjacency_matrix
-
-    # norm_matrix = normalize(adjacency_matrix.astype(np.float32))
-    #mask = ((dismap >= 0.01) * (dismap <= 0.01))
-
-    # mask=(dismap >= 0)
-    # adjacency_matrix = np.where(mask, dismap, 0)
-    # mask=(dismap >= 0)
-    # adjacency_matrix = np.where(mask, 1, dismap)
     
-    return dismap#dismap#norm_matrix
+    
+    return dismap
 
 def esmembedding(sequence_name):
     fea = np.load( "/home/xli/NABProt/task2_NABPs/genfea/RNAfea/rna_esm_fea/" + sequence_name + ".npy")
@@ -204,9 +182,9 @@ allindexnd=np.array(allindex)[:,1:]
 aadict={}
 for i in range(20):
     key=allindexnd[0,i]
-    # print(key)
+    
     values=[float(i) for i in allindexnd[1:,i]]
-    # print(values,len(values))
+    
     aadict[key]=np.array(values)
 
 aadict['X']=np.array([0]*531)
@@ -286,37 +264,26 @@ def w4embedding(seq):
 class ProDataset(Dataset):
     def __init__(self, dataframe):
         self.names = dataframe['name'].values
-        #self.chain=dataframe['chain'].values
-        #self.mutsite = dataframe['Mutsite'].values
+        
         self.labels = dataframe['labels'].values
         self.seqs=dataframe['seq'].values
 
     def __getitem__(self, index):
         sequence_name = self.names[index]#+'_'+self.chain[index]
         seq=self.seqs[index]
-        #mutsite = self.mutsite[index]
-        # label = np.array(self.labels[index])
+       
         label= np.array(list(self.labels[index])).astype('int')#self.labels[index]
         esm_embedding = esmembedding(sequence_name)
         t5_embedding = t5embedding(sequence_name)
         aaindex_fea=aaindexfea(seq)
 
-        #esm_embedding =np.concatenate([esm_embedding, aaindex_fea], axis=1)
-        t5_embedding =np.concatenate([t5_embedding, esm_embedding], axis=1)#,aaindex_fea
-
-        #w1_embedding=t5_embedding#w1embedding(seq)
-        #w4_embedding=w4embedding(seq)
-        w_embedding=aaindex_fea#w1_embedding#w1_embedding#np.concatenate([w1_embedding, w4_embedding], axis=1)#256
-
-        #sequence_embedding = np.concatenate([esm_embedding, bfd_embedding], axis=1)
-
-        graph=load_graph(sequence_name)
-        # sequence_embedding = embedding(sequence_name, sequence, EMBEDDING)
-        # structural_features = get_dssp_features(sequence_name)
         
-        # node_features = np.concatenate([sequence_embedding, structural_features], axis = 1)
-        # graph = load_graph(sequence_name)
-        # print('123')
+        t5_embedding =np.concatenate([t5_embedding, esm_embedding], axis=1)
+
+        
+        w_embedding=aaindex_fea
+        graph=load_graph(sequence_name)
+        
         return sequence_name, esm_embedding,t5_embedding, label , graph,w_embedding #,mutsite
     
     def __len__(self):
@@ -349,13 +316,11 @@ def train_one_epoch(model, data_loader):
 
             graph = Variable(graph)
             y_true = Variable(label)
-            #mutsite=Variable(mutsite)
-
-        #node_features = torch.squeeze(node_features)
+            
         graph = torch.squeeze(graph)
         y_true = torch.squeeze(y_true)
 
-        y_pred = model(esm_features, t5_features,graph,w_embedding)  # y_pred.shape = (L,2)
+        y_pred = model(esm_features, t5_features,graph,w_embedding) 
 
         # calculate loss
         loss = model.criterion(y_pred, y_true)
@@ -463,26 +428,15 @@ def analysis(y_true, y_pred, best_threshold = None):
     }
     return results
 
-LAYER = 8#16#8#8#8#8
-#INPUT_DIM =#2304 #1280#1024#256#1024
+LAYER = 8
 
-#0.930
-# HIDDEN_DIM = 512
-# NUM_CLASSES = 2
-# DROPOUT = 0.2
-# LAMBDA = 1.5
-# ALPHA = 0.7
-# VARIANT = True#
-# LEARNING_RATE = 1E-3 #1E-3#1E-5
-# WEIGHT_DECAY = 0
-
-HIDDEN_DIM = 1024#512
+HIDDEN_DIM = 1024
 NUM_CLASSES = 2
 DROPOUT = 0.1
 LAMBDA = 1.5
 ALPHA = 0.7
-VARIANT = False#True#
-LEARNING_RATE = 1E-3 #1E-3#1E-5
+VARIANT = False
+LEARNING_RATE = 1E-3 
 WEIGHT_DECAY = 0
 
 
@@ -493,7 +447,7 @@ if torch.cuda.is_available():
     torch.cuda.set_device(0)
     torch.cuda.manual_seed(SEED)
 
-#MAP_TYPE = "d"
+
 MAP_TYPE = "c"
 
 trainset=pd.read_csv('/home/xli/NABProt/task2_NABPs/dataprocess/RNArawdataprocess/RNAtrain495.csv')
@@ -514,7 +468,7 @@ for epoch in range(10):
     print('train_loss',epoch_loss_train_avg)
 
     epoch_loss_avg, valid_true, valid_pred, _  = evaluate(model, test_loader)
-    # print('evl_loss',epoch_loss_avg)
+    
     result_valid = analysis(valid_true, valid_pred)#, 0.5
 
     print("Valid loss: ", epoch_loss_avg)
@@ -526,71 +480,3 @@ for epoch in range(10):
     print("Valid AUPRC: ", result_valid['AUPRC'])
     print("Valid mcc: ", result_valid['mcc'])
 
-# trainset=trainset[:10]
-# testset=testset[:10]
-
-# trainset['label'] = trainset['label'].replace(-1, 0)
-
-#train_loader = DataLoader(dataset=ProDataset(trainset), batch_size=1, shuffle=True, num_workers=2)
-
-# testset['label'] = testset['label'].replace(-1, 0)
-
-#test_loader = DataLoader(dataset=ProDataset(testset), batch_size=1, shuffle=True, num_workers=2)
-
-# sequence_names = testset['name'].values
-# sequence_labels = testset['label'].values
-
-
-
-
-
-#train_loader=DataLoader(dataset=ProDataset(train_dataframe), batch_size=1, shuffle=True, num_workers=2)
-#valid_loader=DataLoader(dataset=ProDataset(valid_dataframe), batch_size=1, shuffle=True, num_workers=2)
-# fold=0
-
-# complexarray=np.array(list(trainset['name'].values))
-
-# kfold = KFold(n_splits = 5, shuffle = True)
-
-# fold=0
-# allfoldlist=[]
-# for train_index, valid_index in kfold.split(complexarray):
-#     #print(train_index, valid_index)
-#     model = GraphPPIS(LAYER, HIDDEN_DIM, NUM_CLASSES, DROPOUT, LAMBDA, ALPHA, VARIANT)
-#     if torch.cuda.is_available():
-#         model.cuda()
-#     print("\n\n========== Fold " + str(fold + 1) + " ==========")
-#     tmp_trainset=trainset.iloc[train_index]
-#     tmp_validset=trainset.iloc[valid_index]
-
-#     tmp_train_loader=DataLoader(dataset=ProDataset(tmp_trainset), batch_size=1, shuffle=True, num_workers=2)
-#     tmp_valid_loader=DataLoader(dataset=ProDataset(tmp_validset), batch_size=1, shuffle=True, num_workers=2)
-    
-#     foldlist=[]
-#     for epoch in range(10):
-#         print("\n========== train epoch " + str(epoch + 1) + " ==========")
-#         model.train()
-#         epoch_loss_train_avg = train_one_epoch(model, tmp_train_loader)
-#         print('train_loss',epoch_loss_train_avg)
-
-#         epoch_loss_avg, valid_true, valid_pred, _  = evaluate(model, tmp_valid_loader)
-#         # print('evl_loss',epoch_loss_avg)
-#         result_valid = analysis(valid_true, valid_pred, 0.5)
-
-#         print("Valid loss: ", epoch_loss_avg)
-#         print("Valid binary acc: ", result_valid['binary_acc'])
-#         print("Valid precision: ", result_valid['precision'])
-#         print("Valid recall: ", result_valid['recall'])
-#         print("Valid f1: ", result_valid['f1'])
-#         print("Valid AUC: ", result_valid['AUC'])
-#         print("Valid AUPRC: ", result_valid['AUPRC'])
-#         print("Valid mcc: ", result_valid['mcc'])
-#         result_list=[epoch_loss_avg,result_valid['binary_acc'],result_valid['precision'],result_valid['recall'],result_valid['f1'],result_valid['AUC'],result_valid['AUPRC'],result_valid['mcc']]
-#         print(result_list)
-#         foldlist.append(result_list)
-#     fold+=1
-#     allfoldlist.append(foldlist)
-
-# cv_result=np.array(allfoldlist).mean(axis=0)
-# cv_pd=pd.DataFrame(cv_result,columns=['epoch_loss_avg', 'binary_acc', 'precision', 'recall','f1','AUC','AUPRC','mcc'])
-# cv_pd.to_csv('/home/xli/NABProt/task2_NABPs/DNA/abmodel/newab/cv_dna_model4.csv')
